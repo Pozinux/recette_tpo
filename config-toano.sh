dab7b76d4588e9c0d7110e323c3b7e9aac4f5bd3
Timothé Poznanski <tim.poznanski@gmail.com>
2020-12-24 16:33:08 +0100

    Create config-toano.sh

---
+++ config-toano.sh
@@ -0,0 +1,8 @@
+# A ANONYMISER !!!   
+readonly VCENTER_V5="vcenter_1"
+readonly PUBKEYS=("IS_name_1" "IS_name_2")  # Liste des noms des personnes dans les fichiers .ssh/authorized_keys
+readonly SITE_DNS=("SITE_1"                  "SITE_1"                  "SITE_2"                  "TR2")
+readonly SERV_DNS=("dns_1_site_1"            "dns_2_site_1"            "dns_1_site_2"            "dns__site_2")
+readonly  IP_DNS=("10.XX.XX.XX"            "10.XX.XX.XX"              "10.XX.XX.XX"              "10.XX.XX.XX")
+readonly PATH_TO_DURCISSEMENT_SCRIPT="/partage_name/scripts/linux/durcissement"
+readonly user_ssh_rhel7="toto"
